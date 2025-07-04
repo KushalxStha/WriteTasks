e6300a90e0889badae905e17644230ba647cfd78
Kushal Shrestha <shresthakushal61@gmail.com>
2025-07-04 14:36:01 +0545

    refactor: single form using include

--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -1,45 +1,5 @@
 @extends('layout.app')
 
-@section('style')
-<style>
-    .err-msg{
-        color: red;
-    }
-</style>
-@endsection
-
-@section('title', "Edit Task")
-
 @section('content')
-    <form method="POST" action="{{ route('tasks.update',['task'=> $task->id]) }}">
-    @csrf
-    @method("PUT")
-    <div>
-        <label for="title">Title</label>
-        <input type="text" name="title" id="title" value="{{ $task->title }}">
-        @error('title')
-            <p class="err-msg text-red-500">{{ $message }}</p>
-        @enderror
-    </div>
-
-    <div>
-        <label for="description">Description</label>
-        <textarea type="text" name="description" id="description" rows="5" value="{{ $task->description }}"></textarea>
-        @error('description')
-            <p class="err-msg">{{ $message }}</p>
-        @enderror
-    </div>
-
-    <div>
-        <label for="long_description">Title</label>
-        <textarea type="text" name="long_description" id="long_description" rows="10" value="{{ $task->long_description }}"></textarea>
-        @error('long_description')
-            <p class="err-msg">{{ $message }}</p>
-        @enderror
-    </div>
-
-    <div>
-        <button type="submit">Edit Task</button>
-    </div>
-    </form>
+    @include('form',['task'=> $task])
 @endsection
---
+++ resources/views/form.blade.php
@@ -0,0 +1,47 @@
+@extends('layout.app')
+
+@section('style')
+<style>
+    .err-msg{
+        color: red;
+    }
+</style>
+@endsection
+
+@section('title', isset($task) ? "Edit Task":"Add Task")
+
+@section('content')
+    <form method="post" action="{{ isset($task)? route('tasks.update',['task'=> $task->id]) : route('tasks.store') }}">
+    @csrf
+    @isset($task)
+        @method('PUT')
+    @endisset
+    <div>
+        <label for="title">Title</label>
+        <input type="text" name="title" id="title" value="{{ $task->title ?? old('title') }}">
+        @error('title')
+            <p class="err-msg text-red-500">{{ $message }}</p>
+        @enderror
+    </div>
+
+    <div>
+        <label for="description">Description</label>
+        <textarea type="text" name="description" id="description" rows="5">{{ $task->description ?? old('description') }}</textarea>
+        @error('description')
+            <p class="err-msg">{{ $message }}</p>
+        @enderror
+    </div>
+
+    <div>
+        <label for="long_description">Title</label>
+        <textarea type="text" name="long_description" id="long_description" rows="10">{{ $task->long_description ?? old('long_description') }}</textarea>
+        @error('long_description')
+            <p class="err-msg">{{ $message }}</p>
+        @enderror
+    </div>
+
+    <div>
+        <button type="submit">{{ isset($task)? "Edit Task" : "Add Task" }}</button>
+    </div>
+    </form>
+@endsection
