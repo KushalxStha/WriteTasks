e30468168d1e1a7eb23b44bb7005c84609a4dcf1
Kushal Shrestha <shresthakushal61@gmail.com>
2025-07-06 16:47:04 +0545

    styling lists

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -5,8 +5,7 @@
 @section('content')
 
     <nav class="mb-4">
-        <a href="{{ route('tasks.index') }}"
-        class="font-medium text-gray-700 underline decoration-pink-500">← Go Back to the List</a>
+        <a href="{{ route('tasks.index') }}" class="link">← Go Back to the List</a>
     </nav>
 
     <P class="mb-4 text-slate-700">{{ $task->description }}</P>
@@ -19,32 +18,30 @@
     Created {{ $task->created_at->diffForHumans() }} . Updated {{ $task->updated_at->diffForHumans() }}
     </p>
 
-    @if ($task->completed)
-        <span class="font-medium text-green-500">Completed</span>
-    @else
-        <span class="font-medium text-red-500">Not Completed</span>
-    @endif
+    <p class="mb-4">
+        @if ($task->completed)
+            <span class="font-medium text-green-500">Completed</span>
+        @else
+            <span class="font-medium text-red-500">Not Completed</span>
+        @endif
+    </p>
 
-    <div>
+    <div class="flex gap-2">
         <a href="{{ route('tasks.edit',['task'=> $task]) }}"
-        class="px-2 py-1 text-center font-medium rounded-md shadow-sm ring-1 ring-slate-700/10 hover:bg-slate-50">Edit</a>
-    </div>
+        class="btn">Edit</a>
 
-    <div>
         <form method="POST" action="{{ route('tasks.toggle-complete', ['task'=> $task]) }}">
         @csrf
         @method('PUT')
-        <button type="submit">
+        <button class="btn cursor-pointer" type="submit">
             Mark as {{ $task->completed ? 'not complete': 'complete' }}
         </button>
         </form>
-    </div>
 
-    <div>
         <form action="{{ route("tasks.destroy",['task'=> $task]) }}" method="POST">
         @csrf
         @method('DELETE')
-            <button type="submit">Delete</button>
+            <button class="btn cursor-pointer" type="submit">Delete</button>
         </form>
     </div>
 @endsection
