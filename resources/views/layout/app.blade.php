94686e5fdf2e1a0b4e8dc99b0f12d0c9891f264f
Kushal Shrestha <shresthakushal61@gmail.com>
2025-07-06 18:39:59 +0545

    feat: alpine.js used

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -6,6 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>WriteTasks</title>
 
+    <script src="//unpkg.com/alpinejs" defer></script>
+
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     @yield("style")
 </head>
@@ -14,7 +16,25 @@
     <h1 class="text-2xl mb-4">@yield("title")</h1>
 
     @if (session()->has('success'))
-        <div>{{ session('success') }}</div>
+        <div x-data="{ flash: true }">
+            <div x-show ="flash"
+                class="relative mb-4 rounded border border-green-400 bg-green-100 px-4 py-3 text-lg text-green-700"
+                role="alert">
+                <strong class="font-bold">Success!</strong>
+                <div>
+                    {{ session('success') }}
+                </div>
+
+                <span class="absolute top-0 right-0 px-4 py-3">
+                    <svg xmlns="http://ww.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                        stroke-width="1.5" stroke="currentColor"
+                        class="h-6 w-6 cursor-pointer"
+                        @click="flash=false">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+                    </svg>
+                </span>
+            </div>
+        </div>
     @endif
 
     <div>
